a0da3c94aa45f38aebd6fec8d5f4374f7695fc4a
kaliane <kally_lari@hotmail.com>
2018-03-31 19:08:32 -0300

    Classes DAO

---
+++ SoftTalk/src/br/com/entidades/Setor.java
@@ -0,0 +1,33 @@
+
+package br.com.entidades;
+
+public class Setor {
+    Integer idsetor;
+    String nome, flagativo;
+
+    public Integer getIdsetor() {
+        return idsetor;
+    }
+
+    public void setIdsetor(Integer idsetor) {
+        this.idsetor = idsetor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFlagativo() {
+        return flagativo;
+    }
+
+    public void setFlagativo(String flagativo) {
+        this.flagativo = flagativo;
+    }
+
+    
+}
