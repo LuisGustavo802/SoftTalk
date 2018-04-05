800602cf9c977d3ad8080927c407e62eebae0167
kaliane <kally_lari@hotmail.com>
2018-04-05 02:20:42 -0300

    #CadastroUsuario

--- SoftTalk/src/br/com/login/LoginController.java
+++ SoftTalk/src/br/com/login/LoginController.java
@@ -5,36 +5,19 @@
  */
 package br.com.login;
 
-<<<<<<< HEAD
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-
-/**
- * FXML Controller class
- *
- * @author Kaliane Viesseli
- */
-public class LoginController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
-=======
+import br.com.cadastroUsuario.CadastroUsuario;
+import java.awt.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author luis_
  */
 public class LoginController {
+    
+    @FXML
+    private void entrarCadastro(ActionEvent event){
+        new CadastroUsuario().cadastroUsuario();
+    }
 
->>>>>>> upstream/master
 }
