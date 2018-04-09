0efb33a08b770019581a5f7cdfb2c0a1901dcad3
erasmojunkes <37718439+erasmojunkes@users.noreply.github.com>
2018-04-08 21:49:55 -0300

    Merge Telas

--- SoftTalk/src/br/com/softtalk/SoftTalkController.java
+++ SoftTalk/src/br/com/softtalk/SoftTalkController.java
@@ -1,24 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.softtalk;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-/**
- * FXML Controller class
- *
- * @author erasm
- */
+
 public class SoftTalkController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
