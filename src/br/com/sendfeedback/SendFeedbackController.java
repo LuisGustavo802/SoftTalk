8a7758576ce0065503a823f115e3ba1ad2fee6a0
Paulo Guilherme Santiago <paulo.guilherme.98@hotmail.com>
2018-06-27 01:08:42 -0300

    Ajustes
    Ajuste fxml

--- src/br/com/sendfeedback/SendFeedbackController.java
+++ src/br/com/sendfeedback/SendFeedbackController.java
@@ -106,10 +106,7 @@ public class SendFeedbackController implements Initializable {
             feedback.setDescricao(txaDescricao.getText());
             
 
-            EnviarEmail email = new EnviarEmail();
             email.enviandoEmail(feedback.getEmailDestinatario() ,2);
-
-
             DAOSendFeedback gravaFeedback = new DAOSendFeedback();
             if (gravaFeedback.enviaFeedback(feedback) > 0) {
                 
